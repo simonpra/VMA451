c421d775e2c13b2646e9d541ece228f358de7885
simonpra <simon.pracchinetti@outlook.com>
2026-01-25 22:13:20 +0100

    added TVOC, ECO2, AQI symbol in charset

--- src/VMA451_charset.h
+++ src/VMA451_charset.h
@@ -18,6 +18,9 @@ const uint8_t* getNumber2x5(char c, uint8_t offsetH, uint8_t *out_len);
 extern const uint8_t symbol_degree_3x4[4];
 extern const uint8_t symbol_humidity_3x3[3];
 extern const uint8_t symbol_ppm_2x11[11];
+extern const uint8_t symbol_tvoc_3x14[14];
+extern const uint8_t symbol_eco2_3x13[13];
+extern const uint8_t symbol_aqi_3x10[10];
 
 #ifdef __cplusplus
 }
